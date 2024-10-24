c5487949343ee6af2c1aecfe40ec1640fcc18551
mariambasem <mariomabasem2004@gmail.com>
2024-10-24 09:58:44 +0300

    Update ur.py

--- ur.py
+++ ur.py
@@ -1,1 +1,19 @@
+name: Run Python Script
 
+on: [push]
+
+jobs:
+  build:
+    runs-on: ubuntu-latest
+
+    steps:
+      - name: Checkout code
+        uses: actions/checkout@v3
+        
+      - name: Set up Python
+        uses: actions/setup-python@v3
+        with:
+          python-version: '3.x'
+
+      - name: Run Python script
+        run: python ur.py
